d7bd647a4fe048dbfed508889a41621253c15423
unknown <redoyhasan4585@gmail.com>
2024-05-09 21:31:02 +0400

    added mongo db and get user from env file

--- server-side-repo/server-side/index.js
+++ server-side-repo/server-side/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express()
@@ -21,6 +22,31 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster01.2xfw1xu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/' ,(req ,res)=>{
   res.send('CRUD_JWT_project is running')
